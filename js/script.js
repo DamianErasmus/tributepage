e26b052af738d8651b2db96a813a46a55aaa2b87
AuthoriD <mr.erasmus93@gmail.com>
2021-07-06 14:47:24 +0700

    changes

---
+++ js/script.js
@@ -0,0 +1,192 @@
+//luffy
+const luffyCard = document.getElementById("luffy");
+const luffyFlag = document.getElementById("luffy_flag");
+const luffyText = document.getElementById("luffy_text");
+
+luffyCard.onmouseenter = function () {
+    luffyFlag.style.display = "none";
+    luffyText.style.display = "block";
+}
+luffyCard.onmouseleave = function () {
+    luffyText.style.display = "none";
+    luffyFlag.style.display = "block";
+}
+
+
+
+//zoro
+const zoroCard = document.getElementById("zoro");
+const zoroFlag = document.getElementById("zoro_flag");
+const zoroText = document.getElementById("zoro_text");
+
+zoroCard.onmouseenter = function () {
+    zoroFlag.style.display = "none";
+    zoroText.style.display = "block";
+}
+zoroCard.onmouseleave = function () {
+    zoroText.style.display = "none";
+    zoroFlag.style.display = "block";
+}
+
+
+
+//sanji
+const sanjiCard = document.getElementById("sanji");
+const sanjiFlag = document.getElementById("sanji_flag");
+const sanjiText = document.getElementById("sanji_text");
+
+sanjiCard.onmouseenter = function () {
+    sanjiFlag.style.display = "none";
+    sanjiText.style.display = "block";
+}
+sanjiCard.onmouseleave = function () {
+    sanjiText.style.display = "none";
+    sanjiFlag.style.display = "block";
+}
+
+
+
+//nami
+const namiCard = document.getElementById("nami");
+const namiFlag = document.getElementById("nami_flag");
+const namiText = document.getElementById("nami_text");
+
+namiCard.onmouseenter = function () {
+    namiFlag.style.display = "none";
+    namiText.style.display = "block";
+}
+namiCard.onmouseleave = function () {
+    namiText.style.display = "none";
+    namiFlag.style.display = "block";
+}
+
+
+
+//usopp
+const usoppCard = document.getElementById("usopp");
+const usoppFlag = document.getElementById("usopp_flag");
+const usoppText = document.getElementById("usopp_text");
+
+usoppCard.onmouseenter = function () {
+    usoppFlag.style.display = "none";
+    usoppText.style.display = "block";
+}
+usoppCard.onmouseleave = function () {
+    usoppText.style.display = "none";
+    usoppFlag.style.display = "block";
+}
+
+
+
+//chopper
+const chopperCard = document.getElementById("chopper");
+const chopperFlag = document.getElementById("chopper_flag");
+const chopperText = document.getElementById("chopper_text");
+
+chopperCard.onmouseenter = function () {
+    chopperFlag.style.display = "none";
+    chopperText.style.display = "block";
+}
+chopperCard.onmouseleave = function () {
+    chopperText.style.display = "none";
+    chopperFlag.style.display = "block";
+}
+
+
+
+//franky
+const frankyCard = document.getElementById("franky");
+const frankyFlag = document.getElementById("franky_flag");
+const frankyText = document.getElementById("franky_text");
+
+frankyCard.onmouseenter = function () {
+    frankyFlag.style.display = "none";
+    frankyText.style.display = "block";
+}
+frankyCard.onmouseleave = function () {
+    frankyText.style.display = "none";
+    frankyFlag.style.display = "block";
+}
+
+
+
+//robin
+const robinCard = document.getElementById("robin");
+const robinFlag = document.getElementById("robin_flag");
+const robinText = document.getElementById("robin_text");
+
+robinCard.onmouseenter = function () {
+    robinFlag.style.display = "none";
+    robinText.style.display = "block";
+}
+robinCard.onmouseleave = function () {
+    robinText.style.display = "none";
+    robinFlag.style.display = "block";
+}
+
+
+
+//brook
+const brookCard = document.getElementById("brook");
+const brookFlag = document.getElementById("brook_flag");
+const brookText = document.getElementById("brook_text");
+
+brookCard.onmouseenter = function () {
+    brookFlag.style.display = "none";
+    brookText.style.display = "block";
+}
+brookCard.onmouseleave = function () {
+    brookText.style.display = "none";
+    brookFlag.style.display = "block";
+}
+
+
+
+//jinbe
+const jinbeCard = document.getElementById("jinbe");
+const jinbeFlag = document.getElementById("jinbe_flag");
+const jinbeText = document.getElementById("jinbe_text");
+
+jinbeCard.onmouseenter = function () {
+    jinbeFlag.style.display = "none";
+    jinbeText.style.display = "block";
+}
+jinbeCard.onmouseleave = function () {
+    jinbeText.style.display = "none";
+    jinbeFlag.style.display = "block";
+}
+
+
+
+//vivi
+const viviCard = document.getElementById("vivi");
+const viviFlag = document.getElementById("vivi_flag");
+const viviText = document.getElementById("vivi_text");
+
+viviCard.onmouseenter = function () {
+    viviFlag.style.display = "none";
+    viviText.style.display = "block";
+}
+viviCard.onmouseleave = function () {
+    viviText.style.display = "none";
+    viviFlag.style.display = "block";
+}
+
+
+
+//carrot
+const carrotCard = document.getElementById("carrot");
+const carrotFlag = document.getElementById("carrot_flag");
+const carrotText = document.getElementById("carrot_text");
+
+carrotCard.onmouseenter = function () {
+    carrotFlag.style.display = "none";
+    carrotText.style.display = "block";
+}
+carrotCard.onmouseleave = function () {
+    carrotText.style.display = "none";
+    carrotFlag.style.display = "block";
+}
+
+
+
